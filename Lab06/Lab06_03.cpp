bee1079c9ff8577cf775d7b45542d0b8f678660f
LunaTearR <saengtawanpoopoom@gmail.com>
2022-12-31 15:27:55 +0700

    commit

--- Lab06/Lab06_03.cpp
+++ Lab06/Lab06_03.cpp
@@ -1,11 +1,27 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-    string n;
-    cin >> n;
+    string s,res,word;
+    set<string> str;
+
+    while (getline(cin, s)){
+        if (s.size() <= 200)
+            res += s;
+    }
+    
+    replace_if(res.begin() , res.end() ,[] (const char& c) { return !isalpha(c) ;},' ');
+    transform(res.begin(), res.end(), res.begin(), ::tolower);
+
+    stringstream iss(res);
+    while (iss >> word){
+        str.insert(word);
+    }
+
+    for (auto x : str) {
+        cout << x << "\n";
+    }
 
     return 0;
 }
