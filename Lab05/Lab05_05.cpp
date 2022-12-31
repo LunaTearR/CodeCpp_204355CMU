f1dc0e6c42d877b032f207d4f673e9fc8498e2b0
LunaTearR <saengtawanpoopoom@gmail.com>
2022-12-31 23:30:06 +0700

    commit

--- Lab05/Lab05_05.cpp
+++ Lab05/Lab05_05.cpp
@@ -3,70 +3,151 @@ using namespace std;
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int n,t,com,max_ce;
+    int n,t,com;
     cin >> n >> t;
-    int arr[n];
+    int cur_arr[n];
+    int result[n][t];
     for (int i = 0; i < n; i++){
-        cin >> arr[i];
-        
+        cin >> cur_arr[i];
     }
-    
+
     int i=0,j=0;
     for (int k = 0; k < t; k++){   
         cin >> com;
         if (com == 1){
             cin >> i >> j;
-            if (i <= n && j >= arr[i])
-                arr[i-1] += 1;
-            else
-                continue;
-        } else if (com == 2){
+            if (i <= n && j > cur_arr[i-1])
+                cur_arr[i-1] += 1;
+        } 
+        else if (com == 2){
             cin >> i >> j;
-            if (i <= n && j<= arr[i])
-                arr[i-1] -= 1;
-            else
-                continue;
-        } else if (com == 3){
+            if (i <= n && j <= cur_arr[i-1])
+                cur_arr[i-1] -= 1;
+        } 
+        else if (com == 3){
             cin >> i >> j;
-            // if(i <= n){
-            //     if(arr[i-2] > arr[i-1] - 1){
-            //         if(arr[i-2] - arr[i-1] > 1){
-            //             arr[i-2] -= 3;
-            //         }
-            //         else if(arr[i-2] - arr[i-1] == 1){
-            //             arr[i-2] -= 2;
-            //         }
-            //         else{
-            //             arr[i-2] -= 1;
-            //         }
-            //     }
-            //     if(arr[i] < arr[i-1] - 1){
-            //         continue;
-            //     }
-                // arr[i-1] -= 2;
-
-
-
-            // else
-            //     continue;
-        } else if (com == 4){
-            cin >> i;
-            if (i<=n){
-                for (int i=0;i<n;i++){
-                    if (arr[i] > 0)
-                        arr[i]--;
+            // left
+            if(i == 1){
+                //No. == I
+                if(cur_arr[i-1] <= j - 2){
+                    cur_arr[i-1] = cur_arr[i-1];
                 }
-            } else
-                continue;           
+                else if(cur_arr[i-1] == j - 1){
+                    cur_arr[i-1] -= 1;
+                }
+                else if(cur_arr[i-1] == j){
+                    cur_arr[i-1] -= 2;
+                }
+                else{
+                    cur_arr[i-1] -= 3;
+                }
+                //No. == I + 1
+                if(cur_arr[i] <= j - 2){
+                    cur_arr[i] = cur_arr[i];
+                }
+                else if(cur_arr[i] == j - 1){
+                    cur_arr[i] -= 1;
+                }
+                else if(cur_arr[i] == j){
+                    cur_arr[i] -= 2;
+                }
+                else{
+                    cur_arr[i] -= 3;
+                }
+            }
+            //right
+            else if(i == n){
+                if(cur_arr[i-2] <= j - 2){
+                    cur_arr[i-2] = cur_arr[i-2];
+                }
+                else if(cur_arr[i-2] == j - 1){
+                    cur_arr[i-2] -= 1;
+                }
+                else if(cur_arr[i-2] == j){
+                    cur_arr[i-2] -= 2;
+                }
+                else{
+                    cur_arr[i-2] -= 3;
+                }
+                if(cur_arr[i-1] <= j - 2){
+                    cur_arr[i-1] = cur_arr[i-1];
+                }
+                else if(cur_arr[i-1] == j - 1){
+                    cur_arr[i-1] -= 1;
+                }
+                else if(cur_arr[i-1] == j){
+                    cur_arr[i-1] -= 2;
+                }
+                else{
+                    cur_arr[i-1] -= 3;
+                }
+            }
+            //middle
+            else if(i < n){
+                if(cur_arr[i-2] <= j - 2){
+                    cur_arr[i-2] = cur_arr[i-2];
+                }
+                else if(cur_arr[i-2] == j - 1){
+                    cur_arr[i-2] -= 1;
+                }
+                else if(cur_arr[i-2] == j){
+                    cur_arr[i-2] -= 2;
+                }
+                else{
+                    cur_arr[i-2] -= 3;
+                }
+                if(cur_arr[i-1] <= j - 2){
+                    cur_arr[i-1] = cur_arr[i-1];
+                }
+                else if(cur_arr[i-1] == j - 1){
+                    cur_arr[i-1] -= 1;
+                }
+                else if(cur_arr[i-1] == j){
+                    cur_arr[i-1] -= 2;
+                }
+                else{
+                    cur_arr[i-1] -= 3;
+                }
+                if(cur_arr[i] <= j - 2){
+                    cur_arr[i] = cur_arr[i];
+                }
+                else if(cur_arr[i] == j - 1){
+                    cur_arr[i] -= 1;
+                }
+                else if(cur_arr[i] == j){
+                    cur_arr[i] -= 2;
+                }
+                else{
+                    cur_arr[i] -= 3;
+                }
+            }
+            else{
+                cur_arr[i] = cur_arr[i];
+            }
+        } 
+        else if (com == 4){
+            cin >> j;
+            if(j > 0){
+                for (int l = 0;l < n;l++){
+                    if (cur_arr[l] >= j){
+                        cur_arr[l] -= 1;
+                    }
+                }
+            }
+            else{
+                cur_arr[i] = cur_arr[i];
+            }
         }
-        
+        for(int a = 0;a < n; a++){
+            result[a][k] = cur_arr[a];
+        }
     }
-    for (auto x:arr){
-        cout << x << " ";
-        
-    }
-    cout << endl;
 
-    
+    for (int x = 0; x < t; x++){
+        for(int y = 0;y < n;y++){
+        cout << result[y][x] << " ";
+        }
+        cout << "\n";
+    }
     return 0;
 }
