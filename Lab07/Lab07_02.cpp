615d282065bbec72a63dffb90ba5ed6f560c698a
LunaTearR <saengtawanpoopoom@gmail.com>
2023-01-01 12:41:27 +0700

    commit

--- Lab07/Lab07_02.cpp
+++ Lab07/Lab07_02.cpp
@@ -10,31 +10,60 @@ int main(){
     long long high [n];
     long long distance [n];
 
-    long long a,b;
-    long long sum_ = 0;
-    long long highest = 0;
+    // long long blk_idx = -1;
+    // long long maximum_block;
+    // long long minimum_block;
+    // long long blk_sz = sqrt(n);
+    // long long block_high[n];
+    // long long block_dis[n];
+    long long l,r;
     long long result_high [m];
     long long result_dis [m];
+    long long sum_ = 0;
 
-    for(int i = 0;i < n; i++){
+    for (int i=0; i<n; i++){
         cin >> high[i];
+        // if (i%blk_sz == 0){
+        //     // entering next block incementing block pointer
+        //     maximum_block = high[i];
+        //     blk_idx++;
+        //     block_high[blk_idx] = high[i];
+        // }
+        // if(maximum_block < high[i]){
+        //     block_high[blk_idx] = high[i];
+        // }
     }
-    for(int i = 0;i < n; i++){
+    // blk_idx = -1;
+    for (int i=0; i<n; i++){
         cin >> distance[i];
+        // if (i%blk_sz == 0){
+        //     // entering next block incementing block pointer
+        //     minimum_block = distance[i];
+        //     blk_idx++;
+        //     block_dis[blk_idx] = distance[i];
+        // }
+        // if(minimum_block > distance[i]){
+        //     block_dis[blk_idx] = distance[i];
+        // }
     }
-    for(int i = 0; i < m; i++){
-        cin >> a >> b;
-        for(int j =a; j<= b; j++){
-            highest = max(high[j],highest);
-            sum_ = sum_ + distance[j];
+    int q;
+    for (int i = 0; i < m; i++){
+        cin >> l >> r;
+        q = high[l];
+        while (l <= r ){
+            if(q < high[l])
+                q = high[l];
+            sum_ += distance[l];
+            l += 1;
         }
-        result_high[i] = highest;
+
+        result_high[i] = q;
         result_dis[i] = sum_;
         sum_ = 0;
-        highest = 0;
     }
-    for(int i = 0; i < m;i++){
+    for(int i = 0; i < m; i++){
         cout << result_high[i] << " " << result_dis[i] << "\n";
     }
+
     return 0;
 }
